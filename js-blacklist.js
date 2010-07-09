0c2f2ba3385c341205b7cdc76819988e16657314
Jiayong Ou <jou@orly.ch>
2010-07-09 22:32:16 +0200

    Move blacklist matching to content script

--- js-blacklist.js
+++ js-blacklist.js
@@ -1,13 +1,34 @@
-document.addEventListener("beforeload", beforeLoadListener, true);
+/* Request blacklist regex source from background.html. Have
+ * to do this since we can't communicate with the background page
+ * synchronously and we can't prevent the event from the message
+ * callback. It might be possible that some JS files has started
+ * to load before the event handler is registered.
+ */
+chrome.extension.sendRequest({}, function(regex) {
+	var blacklistRegex, domainRegex;
 
-function beforeLoadListener(event) {
-	chrome.extension.sendRequest({url: event.url, nodeName: event.target.nodeName}, 
-		function(canLoad) {
-			if (!canLoad) {
-				console.log('blocked script ' + event.url);
-				event.preventDefault();
-			}
+	function canLoad(url, nodeName) {
+		// Initialize lazily to be able to attach event listener ASAP
+		if (typeof blacklistRegex == 'undefined') {
+			blacklistRegex = regex && new RegExp(regex, 'i');
 		}
-	);
-}
- 
+		if (!domainRegex) {
+			domainRegex = new RegExp('http://([^/]+)', 'i');
+		}
+
+		if (blacklistRegex && (nodeName === "SCRIPT")) {
+			var domainMatches = url.match(domainRegex),
+				domain = domainMatches && domainMatches[1];
+
+			return !(domain && domain.match(blacklistRegex));
+		}
+		return true;
+	}
+
+	document.addEventListener("beforeload", function(event) {
+		if (!canLoad(event.url, event.target.nodeName)) {
+			console.log('blocked script ' + event.url);
+			event.preventDefault();
+		}
+	}, true);
+});	
